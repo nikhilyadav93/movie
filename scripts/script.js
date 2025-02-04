647efdf83390fa5917c5c85efcf3a5a5b8b45664
nikhilyadav93 <nikhil123ind@gmail.com>
2025-02-04 20:43:53 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,120 @@
+const movies = {
+    index: [
+    { title: "The Shawshank Redemption", poster: "https://lh3.googleusercontent.com/pw/AP1GczM0ZeqdXo0AzHXrkulGRjfh7S180xohRZmgTMeNKe2frbE9_K1FZLzpGmax-EAtHVDTTCmxmNtNmj0o1TfH2QAC184Xzu-A2K33ICHZSa-Uvr6bAQONEQ7SZctH9hvPPOlcmHWWYnUd-cGOJfYzgmk=w200-h300-s-no-gm?authuser=0", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "The ", poster: "", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "     The n            ", poster: "images/images (3).jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "The egeggyegg egegtion", poster: "images/images (4).jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    ],
+    bollywood: [
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    ],
+    hollywood: [
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    ],
+    tollywood: [
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    ],
+    webseries: [
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    { title: "The Shawshank Redemption", poster: "images/images.jpeg", description: "Horror", starcast: "Tim Robbins, Morgan Freeman, Bob Gunton", genres: "drama, horror", quality: "webdl", duration: "198 min", release: "2025-01-24", l480p: "https://www.dropbox.com/scl/fi/fnzedpmd74qjr1q425bxo/Tumbbad-2018-WebRip-720p-Hindi-AAC-5.1-x264-ESub-mkvCinemas.mkv?rlkey=he94v6ab4p0c6dj71yzvu7qov&st=56wki49r&dl=0", l720p: "https://photos.app.goo.gl/B7Eo1HihdoZQzfSDA", l1080p: "notfound.html" },
+    ]
+    };
+    
+    function loadMovies() {
+    const movieListDiv = document.getElementById("movieList");
+    movieListDiv.innerHTML = '';  // Clear existing movies
+    const page = window.location.pathname.split('/').pop().split('.').shift();
+    const movieCategory = movies[page];
+    if (movieCategory) {
+    movieCategory.forEach(movie => {
+    const movieItem = document.createElement("div");
+    movieItem.classList.add("movie-item");
+    movieItem.innerHTML = `
+    <div class="movie">
+    <img src="${movie.poster || 'https://via.placeholder.com/150'}" alt="${movie.title || 'Movie Poster'}">
+    <h3>${movie.title || 'Movie Title'}</h3>
+    <a href="download.html?title=${movie.title}&poster=${movie.poster}&starcast=${movie.starcast}&genres=${movie.genres}&quality=${movie.quality}&duration=${movie.duration}&release=${movie.release}&l480p=${movie.l480p}&l720p=${movie.l720p}&l1080p=${movie.l1080p}">Download</a>
+    </div>
+    `;
+    movieListDiv.appendChild(movieItem);
+    });
+    } else {
+    movieListDiv.innerHTML = "<p>No movies found for this category.</p>"
+    }
+    }
+    
+    
+    function searchMovies() {
+    const query = document.getElementById("searchInput").value.toLowerCase();
+    const movieItems = document.querySelectorAll(".movie-item");
+    movieItems.forEach(item => {
+    const title = item.querySelector("h3").textContent.toLowerCase();
+    if (title.includes(query)) {
+    item.style.display = "block";
+    } else {
+    item.style.display = "none";
+    }
+    });
+    }
+    
+    // Initial movie load for each category page
+    loadMovies();
+    
+    
+    
+    
+    
+    
+    //search suggestion
+    
+    const searchInput = document.getElementById('searchInput');
+    const suggestionList = document.getElementById('suggestionList');
+    const suggestionsContainer = document.querySelector(".suggestions");
+    const availableSuggestions = [
+    "Fateh","Maidaan","Shaitaan","Vedaa","Gladiator 2","Mufasa The Lion King","The Fall Guy",
+    "Pushpa 2","Twisters","Kalki 2898 AD","Lucky Bhaskar","Maharaja",
+    ];
+    searchInput.addEventListener('input', function () {
+    const inputValue = searchInput.value.toLowerCase();
+    const filteredSuggestions = availableSuggestions.filter(item =>
+    item.toLowerCase().startsWith(inputValue)
+    );
+    
+    // Display suggestions
+    if (inputValue && filteredSuggestions.length > 0) {
+    showSuggestions(filteredSuggestions);
+    } else {
+    hideSuggestions();
+    }
+    });
+    
+    function showSuggestions(suggestions) {
+    suggestionList.innerHTML = ''; // Clear previous suggestions
+    
+    suggestions.forEach(suggestion => {
+    const listItem = document.createElement('li');
+    listItem.textContent = suggestion;
+    listItem.addEventListener('click', () => {
+    searchInput.value = suggestion;
+    hideSuggestions();
+    });
+    
+    suggestionList.appendChild(listItem);
+    });
+    suggestionsContainer.classList.add('show');
+    }
+    
+    
+    function hideSuggestions() {
+    suggestionsContainer.classList.remove('show');
+    }
+    
+    //Hide the suggestions on clicking outside of the datalist
+    document.addEventListener('click', function (event) {
+    if (event.target !== searchInput && !suggestionsContainer.contains(event.target)) {
+    hideSuggestions();
+    }
+    });
